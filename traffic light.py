aeb96efc4a0578fa2b2bc972a373f90169c11fb0
Siddarth_patel <siddarthpatel51@gmail.com>
2025-11-25 18:53:28 +0530

    Add files via upload

---
+++ traffic light.py
@@ -0,0 +1,11 @@
+# A Python program to setup traffic lights
+light=input("enter the color of the light:")
+if(light=="red"):
+    print("stop your car")
+
+elif(light=="yellow"):
+    print("slow your car")
+elif(light=="green"):
+    print("you can go")
+else:
+    print("wrong input")
